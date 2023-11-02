b8ab18c37ed66b8c007ea45c1cb8d41376f83088
RaviParmar <raviparmar78822@gmail.com>
2023-11-02 17:25:48 +0530

    Home

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import Eyeglasses  from '../pages/Eyeglasses'
+import NewArrivals from '../pages/NewArrivals'
+import Sunglasses from '../pages/Sunglasses'
+import Prescription from '../pages/Prescription'
+import Login from '../pages/Login'
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <div>
+          <Routes>
+            <Route path='/' element={<Home/>}> </Route> 
+            <Route path='/Sunglasses' element={<Sunglasses/>}></Route>
+            <Route path='/Eyeglasses' element={<Eyeglasses/>}></Route>
+            <Route path='/NewArrivals' element={<NewArrivals/>}></Route>
+            <Route path='/prescription' element={<Prescription/>}></Route>
+            <Route path='/Login' element={<Login/>}></Route>
+        </Routes>
+    </div>
   )
 }
 
