c69a9d56bc88fc5113535c45b91ac8df0df19a22
deepramani05 <deepramani4653@gmail.com>
2023-11-09 19:38:14 +0530

    eyeglass

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -23,9 +23,67 @@ const Cart = () => {
   return (
     <div>
       {Data.map((ele)=>(
-        <div>
-          {ele.title}
+        <div className="cart-main">
+        <div className="title">
+          <h1>SHOPPING CART</h1>
         </div>
+        <div className="product-main">
+          <div className="heading">
+            <div className="product-heading">
+              <p>Product details</p>
+            </div>
+            <div className="other-heading">
+              <p>Quantity</p>
+              <p>Delivery Date</p>
+              <p>Price</p>
+            </div>
+          </div>
+          <div className="product-detail">
+            <div className="product-detail-sub">
+              <div className="product-img">
+                <div className="product-img-sub">
+                  <img
+                    src="https://india.ray-ban.com/media/catalog/product/cache/ea9bc4936e2b624aa5046f798e60eb2b/8/0/8056597662192_1_2.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="product-name">
+                <div className="product-name-sub">
+                  <h1>AVIATOR REVERSE</h1>
+                  <div className="product-desc">
+                    <h6>
+                      Size : <span>59</span>
+                    </h6>
+                    <h6>
+                      Frame : <span>Pilot</span>
+                    </h6>
+                    <h6>
+                      Temple : <span>145</span>
+                    </h6>
+                  </div>
+                </div>
+                <div className="product-remove">
+                  <p>Remove</p>
+                </div>
+              </div>
+            </div>
+            <div className="product-other-detail">
+              <div className="product-qty">
+                <p>+ 1 -</p>
+              </div>
+              <div className="product-delivery">
+                <p>
+                  Estimated Delivery Date <span>Fri Nov 10</span>
+                </p>
+              </div>
+              <div className="product-price">
+                <p>$ 12,490.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       ))}
     </div>
   );
